a4236ee48b2630f55081a7891e8ae62846149dc9
ice-blaze <franketip@gmail.com>
2024-01-26 11:03:59 +0100

    Add beforeunload first draft

--- src/app/nuggets.ts
+++ src/app/nuggets.ts
@@ -1,8 +1,9 @@
-type Tag = `book` | `article` | `video`
-type Topic = `web` | `gaming` | `softskills`
+type Tag = `book` | `article` | `video` | `tip`
+type Topic = `web` | `gaming` | `softskills` | `angular` | `javascript`
 
 export interface WebNugget {
 	id: string
+  // TODO add title
 	content: string
 	link: string
 	tag: Tag[]
@@ -24,6 +25,13 @@ export const webNuggets: ()=> WebNugget[] = () => [
 		link: `https://www.google.com/maps`,
 		tag: [`book`],
 		topic: [`web`],
+	},
+  {
+		id: `beforeunload`,
+		content: `TBD explain 'data can be lost' feature, angular guard deactivate can help but dont covers all cases. beforeunload allows to cover most cases (some exceptions with mobile) but the downside is the message can't be customised. Still, only 10 code lines to have this feature compared to handle it with a framework which couldn't cover all cases (external links need to be coverd without the guard and therefore nasty dom manual changes)`,
+		link: `https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#usage_notes`,
+		tag: [`tip`],
+		topic: [`web`, `angular`, `javascript`],
 	},
 	{
 		id: `calendar`,
