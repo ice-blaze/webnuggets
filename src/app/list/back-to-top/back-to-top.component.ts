2a239cb0a024801bda75ce55c0b50c94b954f87d
ice-blaze <franketip@gmail.com>
2024-05-26 12:35:24 +0200

    fix: back to top only appears when there is a scrollbar

--- src/app/list/back-to-top/back-to-top.component.ts
+++ src/app/list/back-to-top/back-to-top.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core'
+import {
+	Component, DoCheck,
+} from '@angular/core'
 
 @Component({
 	selector: `app-back-to-top`,
@@ -7,7 +9,13 @@ import {Component} from '@angular/core'
 	templateUrl: `./back-to-top.component.html`,
 	styleUrl: `./back-to-top.component.scss`,
 })
-export class BackToTopComponent {
+export class BackToTopComponent implements DoCheck {
+	public scrollExist = false
+
+	public ngDoCheck() {
+		this.scrollExist = document.body.scrollHeight > window.innerHeight
+	}
+
 	public backToTop(): void {
 		scroll(
 			0,
